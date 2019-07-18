b97c990b479fccbbca44c0235781bb740b2a6e14
Vikas Sharma <vikas.sharma@quovantis.com>
2019-07-19 00:47:55 +0530

    Project initial commit

---
+++ vDependencyResolver/DependencyRegister.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace vDependencyResolver
+{
+    public class DependencyRegister : IDependencyRegister
+    {
+        private readonly IServiceCollection serviceCollection;
+
+        public DependencyRegister(IServiceCollection serviceCollection)
+        {
+            this.serviceCollection = serviceCollection;
+        }
+
+        void IDependencyRegister.AddScoped<TService>()
+        {
+            serviceCollection.AddScoped<TService>();
+        }
+
+        void IDependencyRegister.AddScoped<TService, TImplementation>()
+        {
+            serviceCollection.AddScoped<TService, TImplementation>();
+        }
+
+        void IDependencyRegister.AddSingleton<TService>()
+        {
+            serviceCollection.AddSingleton<TService>();
+        }
+
+        void IDependencyRegister.AddSingleton<TService, TImplementation>()
+        {
+            serviceCollection.AddSingleton<TService, TImplementation>();
+        }
+
+        void IDependencyRegister.AddTransient<TService>()
+        {
+            serviceCollection.AddTransient<TService>();
+        }
+
+        void IDependencyRegister.AddTransient<TService, TImplementation>()
+        {
+            serviceCollection.AddTransient<TService, TImplementation>();
+        }
+    }
+}
---
+++ vDependencyResolver/IDependencyRegister.cs
@@ -0,0 +1,23 @@
+﻿namespace vDependencyResolver
+{
+    public interface IDependencyRegister
+    {
+        void AddScoped<TService>() where TService : class;
+
+        void AddScoped<TService, TImplementation>()
+            where TService : class
+            where TImplementation : class, TService;
+
+        void AddSingleton<TService>() where TService : class;
+
+        void AddSingleton<TService, TImplementation>()
+            where TService : class
+            where TImplementation : class, TService;
+
+        void AddTransient<TService>() where TService : class;
+
+        void AddTransient<TService, TImplementation>()
+            where TService : class
+            where TImplementation : class, TService;
+    }
+}
